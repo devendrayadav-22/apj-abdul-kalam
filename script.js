dc5c37072bf500f6f8a2ff14c9a8fc10680a7d05
devedrayadav-22 <devendraydv55@gmail.com>
2026-08-08 07:16:23 +0530

    scroll behaviour and scroll bar add

--- script.js
+++ script.js
@@ -1,11 +1,74 @@
 'use strict'
-const welcomScreen = document.querySelector('#welcome-screen');
+
+const welcomeScreen = document.querySelector('#welcome-screen');
 const mainContainer = document.querySelector('.main-container');
 const revealEle = document.querySelectorAll('.reveal');
+const progressBar = document.querySelector('.progress-bar');
+const percentValue = document.querySelector('#percent-value');
+const scrollBackButton = document.querySelector('.top-scroll');
+// const introContent = document.querySelectorAll('.intro-content');
 
-welcomScreen.addEventListener('animationed', () => {
-    welcomScreen.remove();
+// remove the welcomscreen
+welcomeScreen.addEventListener('animationend', () => {
+    welcomeScreen.remove();
 });
+
+// reveal the scroll behaviour
+const revealOnScroll = function(){
+    revealEle.forEach((ele) => {
+        const {y} = ele.getBoundingClientRect();
+        if(window.innerHeight > y){
+            ele.classList.add('reveal-show');
+        }
+    })
+}
+window.addEventListener('scroll', revealOnScroll);
+revealOnScroll();
+
+// progress bar calculation and all
+const progBar = function(){
+    const currScroll = window.scrollY;
+    const visibleScreen = window.innerHeight;
+    const  totalPageHeight= document.documentElement.scrollHeight;
+    const maxScroll = totalPageHeight - visibleScreen;
+    const scrollPercent = (currScroll / maxScroll) * 100;
+    progressBar.style.width = `${scrollPercent}%`;
+    
+    if(scrollPercent >= 100){
+        progressBar.style.backgroundColor = "green";
+    }
+    else{
+        progressBar.style.backgroundColor = "rgb(151, 26, 26)"
+    }
+
+    percentValue.textContent = Math.trunc(scrollPercent);
+}
+window.addEventListener('scroll', progBar);
+progBar();
+
+
+
+// const revealOnScroll = function(){
+//   revealEle.forEach((ele) => {
+//         const {y} = ele.getBoundingClientRect();
+//         if(window.innerHeight > y){
+//             ele.classList.add('reveal-show')
+//         }
+//     });  
+// }
+// window.addEventListener('scroll', revealOnScroll);
+
+// const revealIntroContent = function(){
+//     introContent.forEach((pra) => {
+//         const {y} = pra.getBoundingClientRect();
+//         if(window.innerHeight > y){
+//             pra.classList.add('reveal-show')
+//         }
+//     })
+// };
+// window.addEventListener('scroll', revealIntroContent);
+// revealIntroContent();
+// revealOnScroll();
 
 
 // setTimeout(()=>{
